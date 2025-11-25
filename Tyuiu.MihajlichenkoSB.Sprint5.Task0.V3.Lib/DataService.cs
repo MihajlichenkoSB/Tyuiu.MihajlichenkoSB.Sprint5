aff4ac1a46e960ea5a2c3e24e6bb0b96a4db24f7
PC <PC@DESKTOP-TGHAQ4V>
2025-11-25 23:24:29 +0500

    Add project files.

---
+++ Tyuiu.MihajlichenkoSB.Sprint5.Task0.V3.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MihajlichenkoSB.Sprint5.Task0.V3.Lib
+{
+    public class DataService : ISprint5Task0V3
+    {
+        public string SaveToFileTextData(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveToFile(double result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveToFileTextData(int x)
+        {
+
+            double y = -0.25 * (Math.Pow(x, 3) - 3 * Math.Pow(x, 2) + 4);
+
+
+            y = Math.Round(y, 3, MidpointRounding.AwayFromZero);
+
+
+            string fileName = "OutPutFileTask0.txt";
+
+
+            string path = Path.GetTempPath();
+
+
+            string filePath = Path.Combine(path, fileName);
+
+
+            File.WriteAllText(filePath, y.ToString(CultureInfo.InvariantCulture));
+
+
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.MihajlichenkoSB.Sprint5.Task0.V3.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint5.Task0.V3.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint5.Task0.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+                    public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
