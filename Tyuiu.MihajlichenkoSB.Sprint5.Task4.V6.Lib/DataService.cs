423f19ed7ba1492d3e2db4c14e7053bfe7163360
Сергей Михайличенко <sm4269698@gmail.com>
2025-11-26 20:28:24 +0500

    Task 4

--- Tyuiu.MihajlichenkoSB.Sprint5.Task4.V6.Lib/DataService.cs
+++ Tyuiu.MihajlichenkoSB.Sprint5.Task4.V6.Lib/DataService.cs
@@ -1,19 +1,19 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System;
+using System.Globalization;
+using System.IO;
 
 namespace Tyuiu.MihajlichenkoSB.Sprint5.Task4.V6.Lib
 {
-    public class DataService : ISprint5Task4V6
+    public class DataService
     {
         public double LoadFromDataFile(string path)
         {
-            string text = File.ReadAllText(path);
-            double x = Convert.ToDouble(text);
-            return x;
-        }
+            string text = File.ReadAllText(path).Trim();
 
-        public double Calculate(double x)
-        {
-            double y = (1 / Math.Cos(x)) + 2.2 * Math.Pow(x, 2);
+            double x = double.Parse(text, CultureInfo.InvariantCulture);
+
+            double y = 1 / Math.Cos(x) + 2.2 * Math.Pow(x, 2);
+
             return Math.Round(y, 3);
         }
     }
