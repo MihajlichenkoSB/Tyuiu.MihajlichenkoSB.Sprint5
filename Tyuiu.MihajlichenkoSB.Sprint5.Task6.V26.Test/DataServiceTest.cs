f2f6f3e8bbd72d67c21523209cc5bfdbcc905fca
Сергей Михайличенко <sm4269698@gmail.com>
2025-11-27 18:21:47 +0500

    Task 6

---
+++ Tyuiu.MihajlichenkoSB.Sprint5.Task6.V26.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint5.Task6.V26.Lib;
+
+namespace Tyuiu.MihajlichenkoSB.Sprint5.Task6.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string path = "/app/data/AssesmentData/C#/Sprint5Task6/InPutDataFileTask6V26.txt";
+
+            int res = ds.LoadFromDataFile(path);
+
+            Assert.AreEqual(expected: res, actual: res);
+        }
+    }
+}
---
+++ Tyuiu.MihajlichenkoSB.Sprint5.Task6.V26/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint5.Task6.V26.Lib;
+
+namespace Tyuiu.MihajlichenkoSB.Sprint5.Task6.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Вариант 26 | Михайличенко С.Б.";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Работа с файлами                                                        *");
+            Console.WriteLine("* Задание 6                                                               *");
+            Console.WriteLine("* Вариант 26                                                              *");
+            Console.WriteLine("* Михайличенко С.Б.                                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+
+            string path = Path.Combine(@"C:\DataSprint5\", "InPutDataFileTask6V26.txt");
+
+            Console.WriteLine("Входной файл: " + path);
+
+            DataService ds = new DataService();
+            int result = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Количество знаков вопроса: " + result);
+            Console.WriteLine();
+        }
+    }
+}
